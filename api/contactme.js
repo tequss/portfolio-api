12d701656b990e65e1685bc65c9a14c90174f69b
tequs <witek10012@wp.pl>
2020-12-23 14:27:25 +0100

    enable cors

--- api/contactme.js
+++ api/contactme.js
@@ -1,6 +1,20 @@
 const nodemailer = require('nodemailer');
 
-module.exports = (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another option
+  // res.setHeader('Access-Control-Allow-Origin', req.header.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const message = req.body.message; 
@@ -37,3 +51,5 @@ module.exports = (req, res) => {
       }
     });
   }
+
+  module.exports = allowCors(handler);
